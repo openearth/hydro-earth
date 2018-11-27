921f754df7d0143e43fdfb5074e43a9571bafcab
gena <gennadiy.donchyts@gmail.com>
2018-11-27 17:15:19 +0100

    remove templates, only REST calls

--- hydroearth/frontend/api_tasks.py
+++ hydroearth/frontend/api_tasks.py
@@ -3,10 +3,9 @@ import datetime
 
 from hydroearth.frontend.app import get_datastore
 from hydroearth.tasks import tasks
-from hydroearth.data import storage
 
-from flask import Blueprint, current_app, redirect, render_template, request, \
-    jsonify, session, url_for, Response, send_from_directory
+from flask import Blueprint, request, \
+    jsonify, session
 
 import google.oauth2.id_token
 import google.auth.transport.requests
@@ -34,7 +33,8 @@ def list():
     print(request.headers)
     print('Authorization: ' + request.headers['Authorization'])
 
-    if request.headers['Authorization'] != 'undefined' and request.headers['Authorization']:
+    if request.headers['Authorization'] != 'undefined' and request.headers[
+        'Authorization']:
         id_token = request.headers['Authorization'].split(' ').pop()
         # claims = google.oauth2.id_token.verify_oauth2_token(id_token,
         #                                                     HTTP_REQUEST)
@@ -53,20 +53,6 @@ def list():
     return jsonify({'tasks': tasks, 'next_page_token': next_page_token})
 
 
-# @api_tasks.route("/")
-# def list():
-#     token = request.args.get('page_token', None)
-#     if token:
-#         token = token.encode('utf-8')
-#
-#     tasks, next_page_token = get_datastore().list(cursor=token)
-#
-#     return render_template(
-#         "list.html",
-#         tasks=tasks,
-#         next_page_token=next_page_token)
-
-
 # [START list_mine]
 @api_tasks.route("/mine")
 # @oauth2.required
@@ -79,26 +65,10 @@ def list_mine():
         user_id=session['profile']['id'],
         cursor=token)
 
-    return render_template(
-        "list.html",
-        tasks=tasks,
-        next_page_token=next_page_token)
+    return jsonify({'tasks': tasks, 'next_page_token': next_page_token})
 
 
 # [END list_mine]
-
-@api_tasks.route("/templates/<type>", methods=['GET'])
-def get_template(type):
-    """
-    Gets options text file for model type.
-    :return:
-    """
-
-    # read model options file from Cloud Storage
-    content = storage.read_file('templates/' + type + '.yaml')
-
-    return Response(content, status=200, mimetype='application/text')
-
 
 # [START add]
 @api_tasks.route('/add', methods=['POST'])
@@ -132,63 +102,29 @@ def add():
 
     return jsonify(task)
 
-    modelTypes = [
-        {'name': 'wflow'},
-        {'name': 'iMOD'}
-    ]
-
-    if request.method == 'POST':
-        data = request.form.to_dict(flat=True)
-
-        data['status'] = 'CREATED'
-
-        data['createdTime'] = datetime.datetime.now() \
-            .strftime('%Y-%m-%d %H:%M:%S')
-
-        # If an image was uploaded, update the data to point to the new image.
-        image_url = upload_image_file(request.files.get('image'))
-
-        if image_url:
-            data['imageUrl'] = image_url
-
-        # If the user is logged in, associate their profile with the new model.
-        if 'profile' in session:
-            data['createdBy'] = session['profile']['displayName']
-            data['createdById'] = session['profile']['id']
-
-        model = get_datastore().create(data)
-
-        # [START enqueue]
-        q = tasks.get_models_queue()
-        q.enqueue(tasks.build_model, model['id'])
-        # [END enqueue]
-
-        return redirect(url_for('.view', id=model['id']))
-
-    return render_template("form.html", action="Add", model={},
-                           modelTypes=modelTypes)
-
 
 # [END add]
 
 
-@api_tasks.route('/<id>/edit', methods=['GET', 'POST'])
-def edit(id):
+@api_tasks.route('/<id>/update', methods=['GET', 'POST'])
+def update(id):
     model = get_datastore().read(id)
 
     if request.method == 'POST':
         data = request.form.to_dict(flat=True)
 
-        image_url = upload_image_file(request.files.get('image'))
+        # image_url = upload_image_file(request.files.get('image'))
 
-        if image_url:
-            data['imageUrl'] = image_url
+        # if image_url:
+        #     data['imageUrl'] = image_url
 
         model = get_datastore().update(data, id)
 
-        return redirect(url_for('.view', id=model['id']))
+        # return redirect(url_for('.view', id=model['id']))
 
-    return render_template("form.html", action="Edit", model=model)
+    # return render_template("form.html", action="Edit", model=model)
+
+    return jsonify({'updated': True})
 
 
 @api_tasks.route('/<id>/delete', methods=['POST'])
@@ -204,29 +140,3 @@ def delete(id):
         return jsonify({'deleted': True})
 
     return jsonify({'deleted': False, 'message': 'Access denied.'})
-
-
-def upload_image_file(file):
-    """
-    Upload the user-uploaded file to Google Cloud Storage and retrieve its
-    publicly-accessible URL.
-    """
-    if not file:
-        return None
-
-    public_url = storage.upload_file(
-        file.read(),
-        file.filename,
-        file.content_type
-    )
-
-    current_app.logger.info(
-        "Uploaded file %s as %s.", file.filename, public_url)
-
-    return public_url
-
-
-@api_tasks.route('/<id>')
-def view(id):
-    model = get_datastore().read(id)
-    return render_template("view.html", model=model)
